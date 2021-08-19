4f5d132f37b1e9deb93a83b4290b4cc2b38c2d65
hugosmoreira <hugo.n4@gmail.com>
2021-08-18 22:49:01 -0700

    .

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import App from "./App";
 import Create from "./Create";
+import SinglePost from "./SinglePost";
 
 const Routes = () => {
   return (
@@ -11,6 +12,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact component={App}></Route>
         <Route path="/create" exact component={Create}></Route>
+        <Route path="/post/:slug" exact component={SinglePost} />
       </Switch>
     </BrowserRouter>
   );
